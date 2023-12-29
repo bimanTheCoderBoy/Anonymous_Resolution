b70fff3aa3e8bb70456a57b1aa5ee415295c6b51
Biman Kumar Das <daskumarbiman2020@gmail.com>
2023-12-30 03:47:00 +0530

    netlify

--- client/anonymous_resolution/src/App.jsx
+++ client/anonymous_resolution/src/App.jsx
@@ -1,15 +1,16 @@
-import './App.css'
-
+import Footer from "./components/footer/footer";
+import { DarkThemeToggle} from "flowbite-react";
+import Nav from "./components/nav/nav";
 function App() {
-  
-
   return (
     <>
-    <h1>
-      Lets make it 
-    </h1>
-      </>
-  )
+      <div className="min-h-screen min-w-screen bg-white dark:bg-slate-800">
+      <Nav/>
+      <DarkThemeToggle className="absolute right-2 top-2 scale-125"/>
+        <Footer/>
+        </div>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ client/anonymous_resolution/src/components/nav/nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Nav({title}) {
+  return (
+    <div className='dark:bg-slate-900 h-16 fixed left-0 right-0 flex justify-center items-center shadow-lg'>
+      {title+"nnjnj"}
+    </div>
+  )
+}
