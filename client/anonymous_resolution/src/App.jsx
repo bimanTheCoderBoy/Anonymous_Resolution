cea46b54a651def9c591f24e8f70b15d3c1ba140
Jyotirmoy Baidya <jyotirmoybaidya408@gmail.com>
2023-12-31 00:42:47 +0530

    Landing Page

--- client/anonymous_resolution/src/App.jsx
+++ client/anonymous_resolution/src/App.jsx
@@ -9,32 +9,38 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import AddPost from "./pages/AddPost";
 import SinglePost from './pages/SinglePost';
-<<<<<<< HEAD
-
-
-=======
 import { useState } from 'react';
-import Login from './pages/Login';
->>>>>>> e54743a6fc91d72d544f5af46bef2f6154e9012f
+import Landing from './pages/Langing';
+// import Login from './pages/Login';
 // import ""
 
-function App() {
+const login = false;
 
+function App() {
   return (
     <>
       <BrowserRouter>
-        <Toaster />
-        <div className="h-screen flex flex-col bg-white dark:bg-slate-800">
-          <Header />
-          <Routes>
-            {/* <Route path="/" element={<Home />} /> */}
-            <Route path="/" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/add-post" element={<AddPost />} />
-            <Route path="/single-post/:id" element={<SinglePost />} />
-          </Routes>
-          <Navs />
-        </div>
+        {
+          login ?
+            <>
+              <Toaster />
+              <div className="h-screen flex flex-col bg-white dark:bg-slate-800">
+                <Header />
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/profile" element={<Profile />} />
+                  <Route path="/add-post" element={<AddPost />} />
+                  <Route path="/single-post/:id" element={<SinglePost />} />
+                </Routes>
+                <Navs />
+              </div>
+            </> :
+            <div className="h-screen flex flex-col">
+              <Routes>
+                <Route path="/" element={<Landing />} />
+              </Routes>
+            </div>
+        }
       </BrowserRouter>
     </>
   );
