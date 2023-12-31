b27dcf4c3c57ab2d13242a31bb20ce1658d2d01c
Biman Kumar Das <daskumarbiman2020@gmail.com>
2024-01-01 04:16:57 +0530

    final5

--- client/anonymous_resolution/vite.config.js
+++ client/anonymous_resolution/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{proxy:{
+    '/api':'https://resoluxe.onrender.com/'
+  }},
   plugins: [react()],
 })
